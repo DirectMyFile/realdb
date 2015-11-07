3525a2392df9a025724899b1afde50051cc18ab0
Kenneth Endfinger <kaendfinger@gmail.com>
2015-11-07 18:32:02 -0500

    Fix loader to make it less jumpy.

--- example/testdb.dart
+++ example/testdb.dart
@@ -6,16 +6,18 @@ main() async {
   var db = new Database.locatedAt("testdb", fileChunkSize: 1024);
   var size = await db.getTableSize("data");
   var i = 0;
-  String msg = "Loaded 0 out of ${size} (0.0%)";
+  String msg = "(0.0%) Loaded 0 out of ${size} (0.0ms)";
   stdout.write(msg);
   var watch = new Stopwatch();
   watch.start();
-  await for (Row row in db.fetchTable("data", unpackData: false)) {
+  await for (Row row in db.fetchTable("data")) {
     i++;
-    stdout.write("\r\x1b[0K");
     var percent = ((i / size) * 100).toStringAsFixed(1);
     var m = "(${percent}%) Loaded ${i} out of ${size} (${watch.elapsedMicroseconds / 1000}ms)";
-    stdout.write(m);
+    if (m.length < msg.length) {
+      m = m.padRight(msg.length);
+    }
+    stdout.write("\r${m}");
     msg = m;
   }
   watch.stop();
